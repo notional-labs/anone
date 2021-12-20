92882c28ba2503dad4a3599248f0818b602a4549
Brenn Hill <brennhill@gmail.com>
2021-12-21 00:44:28 +0700

    Stylings to look like main site

--- vue/src/components/AnOneWelcome/AnOneWelcome.vue
+++ vue/src/components/AnOneWelcome/AnOneWelcome.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sp-component sp-welcome">
-    <div class="sp-box sp-shadow sp-welcome__main">
+    <div class="sp-box sp-shadow">
       <div class="sp-welcome__header">
         <h2>Welcome to Another-1</h2>
       </div>
@@ -8,9 +8,9 @@
         Text
       </p>
       <div class="sp-welcome__btns">
-        <AnButton href="https://docs.starport.network/" target="_blank" type="primary">Documentation</AnButton>
+        <AnButton href="https://another-1.io/" target="_blank" type="primary">About</AnButton>
         <AnButton
-            href="https://github.com/tendermint/vue"
+            href="https://github.com/notional-labs/anone"
             target="_blank"
             type="secondary"
             class="sp-welcome__btns__github"
