41ef18e14700b9d10c7930fa04f2c17c0db88e34
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2022-01-30 11:37:34 +0700

    add basic structure for smart contract

---
+++ anone-cw721/src/contract.rs
@@ -0,0 +1,40 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::{
+    entry_point, Deps, DepsMut, Env, Response, Binary, StdResult,
+    MessageInfo,
+};
+
+use crate::msg::{
+    InstantiateMsg, ExecuteMsg, MigrateMsg, QueryMsg,
+};
+use crate::error::ContractError;
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    _env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn migrate(deps: DepsMut, _env: Env, _msg: MigrateMsg) -> Result<Response, ContractError> {
+    unimplemented!()
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    unimplemented!()
+}
---
+++ anone-cw721/src/error.rs
@@ -0,0 +1,12 @@
+use cosmwasm_std::StdError;
+use thiserror::Error;
+
+/// Never is a placeholder to ensure we don't return any errors
+#[derive(Error, Debug, PartialEq)]
+pub enum Never {}
+
+#[derive(Error, Debug, PartialEq)]
+pub enum ContractError {
+    #[error("{0}")]
+    Std(#[from] StdError),
+}
