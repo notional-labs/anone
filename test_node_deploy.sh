2da1c958096654a7f9bbd0b5bf522c5260d04637
Long MG <77162856+mgialong215@users.noreply.github.com>
2022-02-20 21:06:51 +0700

    Update test_node_deploy.sh

--- test_node_deploy.sh
+++ test_node_deploy.sh
@@ -95,4 +95,4 @@ anoned collect-gentxs
 anoned validate-genesis
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-anoned start --pruning=nothing --log_level $LOGLEVEL --minimum-gas-prices=0.0001uan1
+anoned start --pruning=nothing --log_level $LOGLEVEL --minimum-gas-prices=0.0001uan1 --p2p.laddr tcp://0.0.0.0:2280 --rpc.laddr tcp://0.0.0.0:2281 -grpc.address 0.0.0.0:2282 --grpc-web.address 0.0.0.0:2283
