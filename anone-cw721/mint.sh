224d6dfb9ac2c05154f4061237eeb772393d10fa
nghuyenthevinh2000 <nghuyenthevinh@gmail.com>
2022-03-03 22:24:03 +0700

    generalize bash script

--- anone-cw721/mint.sh
+++ anone-cw721/mint.sh
@@ -6,7 +6,7 @@ CHAINID="anone-testnet-1"
 CONTRACT_DIR="artifacts/anone_cw721.wasm"
 SLEEP_TIME="15s"
 CONTRACT="one1nc5tatafv6eyq7llkr2gv50ff9e22mnf70qgjlv737ktmt4eswrq9jdqcm"
-MINT='{"mint": {"token_id":"test", "owner": "one17f37pgsgrtkq48u5zwhlf6yyjews3k9nldef8q", "token_uri": "https://gateway.pinata.cloud/ipfs/QmcyWkhgA7J7Xx7VHbLntDJvogX4sj2bqmgT3Hy6ctFiLG"}}'
+MINT="{\"mint\": {\"token_id\":\"test\", \"owner\": \"$(anoned keys show $ACCOUNT -a)\", \"token_uri\": \"https://gateway.pinata.cloud/ipfs/QmcyWkhgA7J7Xx7VHbLntDJvogX4sj2bqmgT3Hy6ctFiLG\"}}"
 
 RES=$(anoned tx wasm execute "$CONTRACT" "$MINT" --from "$ACCOUNT" -y --output json --chain-id "$CHAINID" --node "$NODE" --gas 35000000 --fees 875000uan1 -y --output json)
 echo $RES
