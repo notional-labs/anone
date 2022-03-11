dc33223c532e34d9c3ae1971be1d7e285756f9d5
chinhnotional <eyescryptoinsights@gmail.com>
2022-03-11 09:31:37 +0700

    add burn and transfer scripts

---
+++ anone-cw721/scripts/burn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+NODE="http://65.108.128.139:2281"
+ACCOUNT="Developer"
+CHAINID="anone-testnet-1"
+SLEEP_TIME="15s"
+CONTRACT="one1nc5tatafv6eyq7llkr2gv50ff9e22mnf70qgjlv737ktmt4eswrq9jdqcm"
+TOKEN_ID="$1"
+BURN="{\"burn\": {\"token_id\":\"$TOKEN_ID\"}}"
+
+echo $BURN
+
+RES=$(anoned tx wasm execute "$CONTRACT" "$BURN" --from "$ACCOUNT" -y --output json --chain-id "$CHAINID" --node "$NODE" --gas 35000000 --fees 875000uan1 -y --output json)
+echo $RES
+
+TXHASH=$(echo $RES | jq -r .txhash)
+
+echo $TXHASH
+
+# sleep for chain to update
+sleep "$SLEEP_TIME"
+
+RAW_LOG=$(anoned query tx "$TXHASH" --chain-id "$CHAINID" --node "$NODE" -o json | jq -r .raw_log)
+
+echo $RAW_LOG
