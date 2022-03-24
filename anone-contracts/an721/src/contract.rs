191af0d18bb3458faae5dbc1c773447422f0f018
chinhnotional <eyescryptoinsights@gmail.com>
2022-03-24 23:28:56 +0700

    an721 contract

---
+++ anone-contracts/an721/src/contract.rs
@@ -0,0 +1,128 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, StdResult};
+use cw2::set_contract_version;
+
+use sg_std::fees::burn_and_distribute_fee;
+use sg_std::StargazeMsgWrapper;
+
+use crate::ContractError;
+use cw721::ContractInfoResponse;
+use cw721_base::ContractError as BaseError;
+use url::Url;
+
+use crate::msg::{
+    CollectionInfoResponse, ExecuteMsg, InstantiateMsg, QueryMsg, RoyaltyInfoResponse,
+};
+use crate::state::{CollectionInfo, RoyaltyInfo, COLLECTION_INFO};
+
+// version info for migration info
+const CONTRACT_NAME: &str = "crates.io:an-721";
+const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const CREATION_FEE: u128 = 1_000_000_000;
+
+type Response = cosmwasm_std::Response<StargazeMsgWrapper>;
+pub type An721Contract<'a> = cw721_base::Cw721Contract<'a, Empty, StargazeMsgWrapper>;
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn instantiate(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    let fee_msgs = burn_and_distribute_fee(env, &info, CREATION_FEE)?;
+
+    // cw721 instantiation
+    let info = ContractInfoResponse {
+        name: msg.name,
+        symbol: msg.symbol,
+    };
+    An721Contract::default()
+        .contract_info
+        .save(deps.storage, &info)?;
+
+    let minter = deps.api.addr_validate(&msg.minter)?;
+    An721Contract::default()
+        .minter
+        .save(deps.storage, &minter)?;
+
+    // an721 instantiation
+    if msg.collection_info.description.len() > 256 {
+        return Err(ContractError::DescriptionTooLong {});
+    }
+
+    let image = Url::parse(&msg.collection_info.image)?;
+
+    if let Some(ref external_link) = msg.collection_info.external_link {
+        Url::parse(external_link)?;
+    }
+
+    let royalty_info: Option<RoyaltyInfo> = match msg.collection_info.royalty_info {
+        Some(royalty_info) => Some(RoyaltyInfo {
+            payment_address: deps.api.addr_validate(&royalty_info.payment_address)?,
+            share: royalty_info.share_validate()?,
+        }),
+        None => None,
+    };
+
+    deps.api.addr_validate(&msg.collection_info.creator)?;
+
+    let collection_info = CollectionInfo {
+        creator: msg.collection_info.creator,
+        description: msg.collection_info.description,
+        image: msg.collection_info.image,
+        external_link: msg.collection_info.external_link,
+        royalty_info,
+    };
+
+    COLLECTION_INFO.save(deps.storage, &collection_info)?;
+
+    Ok(Response::default()
+        .add_attribute("action", "instantiate")
+        .add_attribute("contract_name", CONTRACT_NAME)
+        .add_attribute("contract_version", CONTRACT_VERSION)
+        .add_attribute("image", image.to_string())
+        .add_messages(fee_msgs))
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn execute(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
+) -> Result<Response, BaseError> {
+    An721Contract::default().execute(deps, env, info, msg)
+}
+
+#[cfg_attr(not(feature = "library"), entry_point)]
+pub fn query(deps: Deps, env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    match msg {
+        QueryMsg::CollectionInfo {} => to_binary(&query_config(deps)?),
+        _ => An721Contract::default().query(deps, env, msg.into()),
+    }
+}
+
+fn query_config(deps: Deps) -> StdResult<CollectionInfoResponse> {
+    let info = COLLECTION_INFO.load(deps.storage)?;
+
+    let royalty_info_res: Option<RoyaltyInfoResponse> = match info.royalty_info {
+        Some(royalty_info) => Some(RoyaltyInfoResponse {
+            payment_address: royalty_info.payment_address.to_string(),
+            share: royalty_info.share,
+        }),
+        None => None,
+    };
+
+    Ok(CollectionInfoResponse {
+        creator: info.creator,
+        description: info.description,
+        image: info.image,
+        external_link: info.external_link,
+        royalty_info: royalty_info_res,
+    })
+}
--- anone-contracts/an721/src/lib.rs
+++ anone-contracts/an721/src/lib.rs
@@ -1,8 +1,6 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub mod contract;
+mod error;
+pub mod msg;
+pub mod state;
+
+pub use crate::error::ContractError;
