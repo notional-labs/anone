c1af72fe29823a3d9b0575f5b605351d546ebdee
chinhnotional <eyescryptoinsights@gmail.com>
2022-04-03 08:16:04 +0700

    new schema

--- anone-contracts/scripts/query.sh
+++ anone-contracts/scripts/query.sh
@@ -6,6 +6,5 @@ CHAINID="anone-testnet-1"
 
 MARKETPLACE_CONTRACT_ADDR="one19vrjp7fll6a729v464wlxr8a2xqhcddc0e5f0gxkv4fcyl8n23us4mr5jv"
 
-QUERY="{\"get_offerings\":{\"sort_listing\":\"price_lowest\", \"index\": 0, \"size\": 5}}"
-QUERY_ALL="{}"
-RES=$(anoned query wasm contract-state smart "$MARKETPLACE_CONTRACT_ADDR" "$QUERY_ALL" --node "$NODE" --chain-id="$CHAINID" --output json | jq --color-output -r)
+QUERY="{\"get_offerings\":{\"sort_listing\":\"price_lowest\"}}"
+RES=$(anoned query wasm contract-state smart "$MARKETPLACE_CONTRACT_ADDR" "$QUERY" --node "$NODE" --chain-id="$CHAINID" --output json | jq --color-output -r)
