fa1f40d8c3b892c1659f0a84c44e383ec3199eb5
chinhnotional <eyescryptoinsights@gmail.com>
2022-04-04 10:26:30 +0700

    replace juno contract by anone contract

--- anone-contracts/scripts/create_sale.sh
+++ anone-contracts/scripts/create_sale.sh
@@ -7,12 +7,12 @@ SLEEP_TIME="10s"
 
 MARKETPLACE_CONTRACT_ADDR="one1lqgdq9u8zhcvwwwz3xjswactrtq6qzptmlzlh6xspl34dxq32uhqg24m03"
 CW721_CONTRACT_ADDR="one1nc5tatafv6eyq7llkr2gv50ff9e22mnf70qgjlv737ktmt4eswrq9jdqcm"
-INSERT_CW20_CONTRACT_ADDR="juno1hthjpmmjdeev6hkgvfzve0r2kvel5ca3ecr3x6y5t5r24xl4jprsy06v5d"
-INSERT_AMOUNT_WITHOUT_DENOM=500
+INSERT_CW20_CONTRACT_ADDR="one1p4lqunauqgstt6ydszx59y3pg2tkaxlnujl9m5ldz7nqcrn6tjzqhe772z"
+INSERT_AMOUNT_WITHOUT_DENOM=5000
 TOKEN_ID="$1"
 
 # This msg is BASE64_ENCODED_JSON --> { "list_price": { "address": "<INSERT_CW20_CONTRACT_ADDR>", "amount": "<INSERT_AMOUNT_WITHOUT_DENOM>" }} <--
-BASE64_ENCODED_JSON="IHsibGlzdF9wcmljZSI6IAogICAgeyAiYWRkcmVzcyI6ICJqdW5vMWh0aGpwbW1qZGVldjZoa2d2Znp2ZTByMmt2ZWw1Y2EzZWNyM3g2eTV0NXIyNHhsNGpwcnN5MDZ2NWQiLAogICAgImFtb3VudCI6ICI1MDAifQogfQ"
+BASE64_ENCODED_JSON="IHsibGlzdF9wcmljZSI6IAogICAgeyAiYWRkcmVzcyI6ICJvbmUxcDRscXVuYXVxZ3N0dDZ5ZHN6eDU5eTNwZzJ0a2F4bG51amw5bTVsZHo3bnFjcm42dGp6cWhlNzcyeiIsCiAgICAiYW1vdW50IjogIjUwMDAifQogfQ=="
 
 ALL_NFTS_QUERY="{\"all_tokens\": {}}"
 OWNER_OF=$(anoned query wasm contract-state smart "$CW721_CONTRACT_ADDR" "$ALL_NFTS_QUERY" --node "$NODE" --output json)
