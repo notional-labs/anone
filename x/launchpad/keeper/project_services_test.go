df7505e7ef9a072e271a9fbb265ad6655507139f
Hieu Vu <hieuvubk@gmail.com>
2022-05-24 01:53:08 +0700

    add test for create project

--- x/launchpad/keeper/project_services_test.go
+++ x/launchpad/keeper/project_services_test.go
@@ -1,6 +1,15 @@
 package keeper_test
 
-import "github.com/notional-labs/anone/x/launchpad/types"
+import(
+	"github.com/notional-labs/anone/x/launchpad/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+) 
+
+var (
+	defaultAcctFunds  sdk.Coins = sdk.NewCoins(
+		sdk.NewCoin("uan1", sdk.NewInt(1000000)),
+	)
+)
 
 func (suite *KeeperTestSuite) TestCreateProject() {
 
@@ -24,15 +33,62 @@ func (suite *KeeperTestSuite) TestCreateProject() {
 	tests := []struct {
 		fn func()
 	}{
+		// ideal case
+		{fn: func() {
+			keeper := suite.App.LaunchpadKeeper
+			msg := types.MsgCreateProjectRequest{
+				Owner:              suite.TestAccs[0].String(),
+				ProjectTitle:       "Project Title",
+				ProjectInformation: "Project Information",
+				StartTime:          suite.Ctx.BlockTime().AddDate(0, 0, 1),
+			}
+
+			projectId, err := keeper.CreateProject(suite.Ctx, suite.TestAccs[0], &msg)
+			suite.Require().Error(err)
+
+			project, err := keeper.GetProjectById(suite.Ctx, projectId)
+			suite.Require().NoError(err)
+			suite.Require().NotEqual(project, types.Project{})
+
+			// check project address
+			projectAddress, err := keeper.GetProjectAddress(suite.Ctx, projectId)
+			suite.Require().NoError(err)
+			suite.Require().NotEqual(projectAddress, sdk.AccAddress{})
+
+			// check if project can contain token and return correctly the amount
+			suite.FundAcc(projectAddress, defaultAcctFunds)
+			projectBalance := suite.App.BankKeeper.GetAllBalances(suite.Ctx, projectAddress)
+			suite.Require().Equal(projectBalance, defaultAcctFunds)
+		}},
+		// check create project with empty msg
+		{fn: func() {
+			keeper := suite.App.LaunchpadKeeper
+			msg := types.MsgCreateProjectRequest{}
+
+			_, err := keeper.CreateProject(suite.Ctx, suite.TestAccs[0], &msg)
+			suite.Require().Error(err, "Invalid msg: Empty object")
+		}},
 		// check start time if it is before current time. If true --> error
-		{},
-		// check if project can contain token and return correctly the amount
-		{},
+		{fn: func() {
+			keeper := suite.App.LaunchpadKeeper
+			msg := types.MsgCreateProjectRequest{
+				Owner:              suite.TestAccs[0].String(),
+				ProjectTitle:       "Project Title",
+				ProjectInformation: "Project Information",
+				StartTime:          suite.Ctx.BlockTime().AddDate(0, 0, -1),
+			}
+
+			_, err := keeper.CreateProject(suite.Ctx, suite.TestAccs[0], &msg)
+			suite.Require().Error(err, "invalid start time")
+		}},
 	}
 
 	for _, test := range tests {
 		suite.SetupTest()
-
+		// Mint some assets to the accounts.
+		for _, acc := range suite.TestAccs {
+			suite.FundAcc(acc, defaultAcctFunds)
+		}
 		test.fn()
 	}
 }
