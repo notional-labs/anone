4b4687c98b613f5c56e703e05e8bd9c353e27d6a
Hieu Vu <hieuvubk@gmail.com>
2022-05-25 02:45:37 +0700

    add grpc query

--- x/launchpad/keeper/grpc_query.go
+++ x/launchpad/keeper/grpc_query.go
@@ -4,6 +4,9 @@ import (
 	"context"
 
 	"github.com/notional-labs/anone/x/launchpad/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var _ types.QueryServer = Querier{}
@@ -17,13 +20,33 @@ func NewQuerier(k Keeper) Querier {
 }
 
 func (q Querier) Params(ctx context.Context, req *types.QueryParamsRequest) (*types.QueryParamsResponse, error) {
-	return &types.QueryParamsResponse{}, types.ErrNotImplemented
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	context := sdk.UnwrapSDKContext(ctx)
+	return &types.QueryParamsResponse{Params: q.Keeper.GetParams(context)}, nil
 }
 
 func (q Querier) Project(ctx context.Context, req *types.ProjectRequest) (*types.ProjectResponse, error) {
-	return &types.ProjectResponse{}, types.ErrNotImplemented
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	context := sdk.UnwrapSDKContext(ctx)
+	project, err := q.Keeper.GetProjectById(context, req.ProjectId)
+	if(err != nil) {
+		return nil, err
+	}
+	return &types.ProjectResponse{Project: &project}, nil
 }
 
 func (q Querier) TotalProject(ctx context.Context, req *types.TotalProjectRequest) (*types.TotalProjectResponse, error) {
-	return &types.TotalProjectResponse{}, types.ErrNotImplemented
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	context := sdk.UnwrapSDKContext(ctx)
+	projectIds, err := q.Keeper.GetAllProjects(context)
+	if(err != nil) {
+		return nil, err
+	}
+	return &types.TotalProjectResponse{ProjectsId: projectIds}, nil
 }
