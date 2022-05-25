8ad92111bdb43f89220104895f4818bee0e29abf
Sotatek-HieuVu2 <hieuvubk@gmail.com>
2022-05-26 02:13:47 +0700

    add handler

--- x/launchpad/handler.go
+++ x/launchpad/handler.go
@@ -11,12 +11,25 @@ import (
 
 // NewHandler ...
 func NewHandler(k keeper.Keeper) sdk.Handler {
+	msgServer := keeper.NewMsgServerImpl(k)
 	// this line is used by starport scaffolding # handler/msgServer
 
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
+		case *types.MsgCreateProjectRequest:
+			res, err := msgServer.CreateProject(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgModifyProjectInformationRequest:
+			res, err := msgServer.ModifyProjectInformation(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgModifyStartTimeRequest:
+			res, err := msgServer.ModifyStartTime(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgDeleteProjectRequest:
+			res, err := msgServer.DeleteProject(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		// this line is used by starport scaffolding # 1
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
