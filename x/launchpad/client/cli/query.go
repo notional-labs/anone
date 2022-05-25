bd76ee47b11ff50c30a4a265cb1f4228dfdb5860
Sotatek-HieuVu2 <hieuvubk@gmail.com>
2022-05-26 02:41:04 +0700

    add query cmd to root

--- x/launchpad/client/cli/query.go
+++ x/launchpad/client/cli/query.go
@@ -27,6 +27,8 @@ func GetQueryCmd(queryRoute string) *cobra.Command {
 	}
 
 	cmd.AddCommand(CmdQueryParams())
+	cmd.AddCommand(GetCmdQueryProject())
+	cmd.AddCommand(GetCmdQueryProjects())
 	// this line is used by starport scaffolding # 1
 
 	return cmd
